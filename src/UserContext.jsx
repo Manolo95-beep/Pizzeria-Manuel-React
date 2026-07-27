c95f5671f71e492432fd7ab10a7067ec5688c63e
MANUEL MUÑOZ <munozoleamanuel@gmail.com>
2026-07-27 17:29:10 -0400

    1eravance

--- src/UserContext.jsx
+++ src/UserContext.jsx
@@ -4,21 +4,44 @@ export const UserContext = createContext()
 
 export const UserProvider = ({children}) => {
 
-    const [token, setToken] = useState(true)
+    const [token, setToken] = useState(localStorage.getItem("token") || "");
+    const [email, setEmail] = useState(localStorage.getItem("email") || "");
+
+    const login = async(email,password) =>{
+
+    }
+
+    const register = async(email,password) =>{
+        
+    }
+
+
 
     const logout = () => {
-        setToken (false)
+
+        setToken("")
+        setEmail("")
+
+        localStorage.removeItem("token")
+        localStorage.removeItem("email")
+
     }
 
-    const login = () => {
-        setToken(true)
+    const getProfile = async ()=>{
+
     }
+
+
+    
   return (
     <UserContext.Provider
         value={{
             token,
+            email,
+            login,
+            register,
             logout,
-            login
+            getProfile
         }}
         >
             {children}
